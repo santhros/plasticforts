4221489c6c3e8ccf296104a6be1422c6ffd9af02
santhros <cadcompsa@gmail.com>
2023-04-05 16:06:12 -0500

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -5,6 +5,12 @@ PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 SERVICES_OFFERED=$($PSQL "SELECT service_id, name FROM services")
 
 MAIN_MENU() {
+    if [[ $1 ]]
+    then
+        echo -e "\n$1"
+    else
+        echo -e "\nWelcome to our Beauty Saloon, what would you like?"
+    fi
     #echo -e "Please choose one of the following services:\n"
     echo "$SERVICES_OFFERED" | while read SERVICE_ID BAR NAME
     do
@@ -16,14 +22,20 @@ MAIN_MENU() {
     # Evaluate if service is null
     if [[ -z $SERVICE_NAME ]]
     then
-          MAIN_MENU
+          MAIN_MENU "The service you chose does not exist, try again."
+    else
+          MAKE_APPOINTMENT $SERVICE_ID_SELECTED
     fi
+}
+
+MAKE_APPOINTMENT() {
+    # Extract the service id chosen
+    SERVICE_ID_SELECTED=$1
     # Ask for the phone number
     echo -e "Please enter your phone number."
     read CUSTOMER_PHONE
     # Evaluate if the customer phone exists in the table customers
     CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
-    echo "El nombre es $CUSTOMER_NAME"
     if [[ -z $CUSTOMER_NAME ]] 
     then
           echo -e "Please enter your name."
@@ -32,9 +44,7 @@ MAIN_MENU() {
           INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES ('$CUSTOMER_PHONE','$CUSTOMER_NAME')")
     fi
     # Let's get the customer id
-    echo "El customer NAME es $CUSTOMER_NAME"
     CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
-    echo "El customer ID es $CUSTOMER_ID"
     echo -e "Please enter your time for the appointment."
     read SERVICE_TIME
     INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
@@ -42,3 +52,4 @@ MAIN_MENU() {
 }
 
 MAIN_MENU
+exit
